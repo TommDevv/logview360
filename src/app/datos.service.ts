744fd98eaaab01d6c893ba8f008cc66f2460a5f5
TommDevv <tomasonic2@gmail.com>
2025-05-30 16:30:26 -0500

    hola

---
+++ src/app/datos.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DatosService {
+ urlAPI = 'http://localhost:8000'
+  constructor(private http: HttpClient) { }
+
+  cargarRegistros(): Observable<any> {
+    return this.http.get(`${this.urlAPI}/obtener/todos`);
+  }
+
+  cargarIndividual(id:string): Observable<any>{
+    return this.http.get(`${this.urlAPI}/obtener/transaction_id/${id}`);
+  }
+}
